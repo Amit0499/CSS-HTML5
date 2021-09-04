5bc2970038fb40ed61c5e3beda3c837e87914b08
Amit <63177153+Amit0499@users.noreply.github.com>
2021-09-04 07:25:05 +0530

    Add files via upload

---
+++ 1-nestedLoop.js
@@ -0,0 +1,12 @@
+  
+// Using javascript demonstrate Nested loop.
+
+/*arr = [['hello', 'world!'], ['How', 'are', 'you', 'doing?']]
+
+for (i = 0; i < arr.length; i++) {
+    for (j = 0; j < arr[i].length; j++) {
+        console.log(arr[i][j]);
+    }
+}
+*/
+console.log('hello world!!');
---
+++ index.js
@@ -0,0 +1,128 @@
+function formValidation() {
+    var uid = document.registration.userid;
+    var passid = document.registration.passid;
+    var uname = document.registration.username;
+    var uadd = document.registration.address;
+    var ucountry = document.registration.country;
+    var uzip = document.registration.zip;
+    var uemail = document.registration.email;
+    var creditCard = document.registration.creditCard;
+    var umsex = document.registration.msex;
+    var ufsex = document.registration.fsex; if (userid_validation(uid, 5, 12)) {
+        if (passid_validation(passid, 7, 12)) {
+            if (allLetter(uname)) {
+                if (alphanumeric(uadd)) {
+                    if (countryselect(ucountry)) {
+                        if (allnumeric(uzip)) {
+                            if (ValidateCreditCard(creditCard)) {
+                                if (ValidateEmail(uemail)) {
+                                    if (validsex(umsex, ufsex)) {
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return false;
+
+} function userid_validation(uid, mx, my) {
+    var uid_len = uid.value.length;
+    if (uid_len == 0 || uid_len >= my || uid_len < mx) {
+        alert("User Id should not be empty / length be between " + mx + " to " + my);
+        uid.focus();
+        return false;
+    }
+    return true;
+}
+function passid_validation(passid, mx, my) {
+    var passid_len = passid.value.length;
+    if (passid_len == 0 || passid_len >= my || passid_len < mx) {
+        alert("Password should not be empty / length be between " + mx + " to " + my);
+        passid.focus();
+        return false;
+    }
+    return true;
+}
+function allLetter(uname) {
+    var letters = /^[A-Za-z]+$/;
+    if (1) {
+        return true;
+    }
+    else {
+        alert('Username must have alphabet characters only');
+        uname.focus();
+        return false;
+    }
+}
+function alphanumeric(uadd) {
+    var letters = /^[a-z\d\-_\s]+$/i;
+    if (uadd.value.match(letters)) {
+        return true;
+    }
+    else {
+        alert('User address must have alphanumeric characters only');
+        uadd.focus();
+        return false;
+    }
+}
+function countryselect(ucountry) {
+    if (ucountry.value == "Default") {
+        alert('Select your country from the list');
+        ucountry.focus();
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+function allnumeric(uzip) {
+    var numbers = /^[0-9]+$/;
+    if (uzip.value.match(numbers)) {
+        return true;
+    }
+    else {
+        alert('ZIP code must have numeric characters only');
+        uzip.focus();
+        return false;
+    }
+}
+function ValidateEmail(uemail) {
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (uemail.value.match(mailformat)) {
+        return true;
+    }
+    else {
+        alert("You have entered an invalid email address!");
+        uemail.focus();
+        return false;
+    }
+} function validsex(umsex, ufsex) {
+    x = 0;
+
+    if (umsex.checked) {
+        x++;
+    } if (ufsex.checked) {
+        x++;
+    }
+    if (x == 0) {
+        alert('Select Male/Female');
+        umsex.focus();
+        return false;
+    }
+    else {
+        alert('Form Succesfully Submitted');
+        window.location.reload()
+        return true;
+    }
+}
+function ValidateCreditCard(creditCard) {
+    check = /^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/.test(creditCard.value)
+    if (check) {
+        return true
+    }
+    else {
+        return false
+    }
